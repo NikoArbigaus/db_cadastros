248025f81d1cd07bd097752e238d6b0167b9b6f7
RES0037527 <RES0037527@SES488467.educa.sf>
2024-12-04 16:37:52 -0300

    primeira atualizacao

---
+++ SuperOs/src/superos/superosDAO/ConexaoDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superos.superosDAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexaoDAO {
+    Connection conn;
+    
+    private static final String url = "";
+    private static final String user = "";
+    private static final String pass = "";
+    
+    public Connection conectaBD(){
+        
+    }
+}
